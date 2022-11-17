973123ecf52ecb9ea78827b38aecb929cb1ed177
ElmaSarajlic <116113967+ElmaSarajlic@users.noreply.github.com>
2022-11-17 17:24:14 +0100

    Add files via upload

---
+++ lab4/task3.java
@@ -0,0 +1,20 @@
+package lab4;
+public class task3 {
+    public static void main(String[] args) {
+        LyyraCard cardPekka = new LyyraCard(20);
+        LyyraCard cardBrian = new LyyraCard(30);
+        cardPekka.payGourmet();
+        cardBrian.payEconomical();
+        System.out.println("Pekka: The card has "+ cardPekka +" euros.");
+        System.out.println("Brian: The card has "+ cardBrian +" euros.");
+        cardPekka.loadMoney(20);
+        cardBrian.payGourmet();
+        System.out.println("Pekka: The card has "+ cardPekka +" euros.");
+        System.out.println("Brian: The card has "+ cardBrian +" euros.");
+        cardPekka.payEconomical();
+        cardPekka.payEconomical();
+        cardBrian.loadMoney(50);
+        System.out.println("Pekka: The card has "+ cardPekka +" euros.");
+        System.out.println("Brian: The card has "+ cardBrian +" euros.");
+    }   
+}
